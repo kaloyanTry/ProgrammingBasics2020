21bfc0a61b9322aee93132d6ee07ebae7d80b3b8
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2020-12-30 19:13:31 +0200

    Drawing Strawberry
    Drawing strawberry pattern with 3 for loops and two new string in each

---
+++ MyOld/Strawberry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Strawberry
+{
+    class Strawberry
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int width = 2 * n + 3;
+            int dotCount = 1;
+
+            for (int i = 0; i < n / 2; i++)
+            {
+                Console.WriteLine("{1}\\{0}|{0}/{1}", new string('-', n - i * 2), new string('-', i * 2));
+            }
+  
+            for (int i = 0; i < n /  2 + 1; i++)
+            {
+                Console.WriteLine("{0}#{1}#{0}", new string('-', n - i * 2), new string('.', dotCount));
+                dotCount += 4;
+            }
+
+            for (int i = 0; i <= n; i++)
+            {
+                Console.WriteLine("{0}#{1}#{0}", new string('-', i), new string('.', width - 2 - i * 2));
+            }
+        }
+    }
+}
