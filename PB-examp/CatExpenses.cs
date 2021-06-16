9a14b82c6ff873450920f77d0c920f957869ef8f
Kaloyan Ganchev <17066741+kaloyanTry@users.noreply.github.com>
2021-06-16 20:53:23 +0300

    Update CatExpenses.cs

--- PB-examp/CatExpenses.cs
+++ PB-examp/CatExpenses.cs
@@ -14,8 +14,11 @@ namespace ConsoleApp1
             double priceFood = priceToilet + priceToilet * 0.25;
             double priceToys = priceFood - priceFood * 0.5;
             double priceVet = priceToys + priceToys * 0.1;
+         
             totalCost = priceFood + priceToilet + priceVet + priceToys;
+            
             double extra = totalCost * 0.05;
+            
             totalCost = priceBed + 12 * totalCost + 12 * extra;
 
             Console.WriteLine($"{totalCost:F2} lv.");
